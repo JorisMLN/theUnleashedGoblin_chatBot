309d67baeb04220038560023bc33ea72b07abd7a
Joris <monleon.joris@gmail.com>
2022-12-20 16:22:52 +0100

    Init the bot

---
+++ bot.js
@@ -0,0 +1,12 @@
+const tmi = require('tmi.js');
+
+// Define configuration options
+const options = {
+  identity: {
+    username: 'TheUnleashedGoblin',
+    password: '<OAUTH_TOKEN>'
+  },
+  channels: [
+    '<CHANNEL_NAME>'
+  ]
+};
